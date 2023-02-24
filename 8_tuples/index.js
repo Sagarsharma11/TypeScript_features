73e08c474f963a944ed320c383895b001aad3c57
fx900-fx97@7 <ss365493@gmail.com>
2023-02-24 11:30:35 +0530

    tuple in TS

---
+++ 8_tuples/index.js
@@ -0,0 +1,5 @@
+var User = ["sagar", 53];
+var Person;
+// order is matter in tuples
+Person = ['string', 53, true];
+var rgb = [244, 12, 74];
---
+++ 8_tuples/index.ts
@@ -0,0 +1,6 @@
+const User : (string | number)[] = ["sagar",53]
+let Person : [string, number, boolean]
+// order is matter in tuples
+Person = ['string',53,true]
+const rgb : [number,number,number] = [244,12,74]
+
